1acdc08ed71b7a725a89651bcf7263f43432d6a6
Abraham Gutierrez <labraham1986@gmail.com>
2024-04-27 00:20:43 -0500

    feat: add rounting to page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path:'',
+    loadComponent: () => import('./shared/page/not-found-page/not-found-page.component').then( c => c.NotFoundPageComponent ),
+  },
+  {
+    path:'not-found',
+    loadComponent: () => import('./shared/page/not-found-page/not-found-page.component').then( c => c.NotFoundPageComponent ),
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  }
 ];
